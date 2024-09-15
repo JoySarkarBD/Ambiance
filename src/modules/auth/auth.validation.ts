39406ef97263d3feb2ac9414c211263663e7340b
JoySarkarBD <developer.joysarkar@gmail.com>
2024-09-15 21:46:11 +0600

    chore: zod validation fixed

--- src/modules/auth/auth.validation.ts
+++ src/modules/auth/auth.validation.ts
@@ -8,24 +8,26 @@ import zodErrorHandler from '../../handlers/zod-error-handler';
 const zodAuthSchema = z
   .object({
     email: z
-      .string({ required_error: 'Email page is required' })
-      .email({ message: 'Invalid email address' })
+      .string({ required_error: 'Email page is required.' })
+      .email({ message: 'Invalid email address.' })
       .trim(),
     password: z
-      .string({ required_error: 'Password is required' })
-      .min(6, { message: 'Password must be at least 6 characters long' })
+      .string({ required_error: 'Password is required.' })
+      .min(6, { message: 'Password must be at least 6 characters long.' })
       .trim(),
-    first_name: z.string({ required_error: 'First name is required' }).min(1).trim(),
-    last_name: z.string({ required_error: 'Last name is required' }).min(1).trim(),
+    first_name: z.string({ required_error: 'First name is required.' }).min(1).trim(),
+    last_name: z.string({ required_error: 'Last name is required.' }).min(1).trim(),
     previous_password: z
-      .string({ required_error: 'Previous password is required' })
-      .min(6, { message: 'Previous password must be at least 6 characters long' }),
+      .string({ required_error: 'Previous password is required.' })
+      .min(6, { message: 'Previous password must be at least 6 characters long.' }),
     new_password: z
-      .string({ required_error: 'New password is required' })
-      .min(6, { message: 'New password must be at least 6 characters long' }),
+      .string({ required_error: 'New password is required.' })
+      .min(6, { message: 'New password must be at least 6 characters long.' }),
     resetPasswordToken: z
-      .string({ required_error: 'Reset password token is required' })
-      .uuid({ message: 'Invalid rest password token format' }),
+      .string({
+        required_error: 'Reset password token is required and it should pass by the request query.',
+      })
+      .uuid({ message: 'Invalid rest password token format.' }),
   })
   .strict();
 
