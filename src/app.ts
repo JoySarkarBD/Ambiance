8927f170a01506e02906cbba1f58be7a7a48fffc
JoySarkarBD <developer.joysarkar@gmail.com>
2024-09-21 14:21:16 +0600

    chore: code initialization updated

--- src/app.ts
+++ src/app.ts
@@ -37,7 +37,7 @@ app.use(morgan('dev'));
 // Request Rate Limiting
 const limiter = rateLimit({
   windowMs: config.REQUEST_LIMIT_TIME,
-  max: process.env.NODE_ENV === 'production' ? config.REQUEST_LIMIT_NUMBER : Infinity, // unlimited in development
+  max: process.env.NODE_ENV !== 'production'? Infinity : config.REQUEST_LIMIT_NUMBER , // unlimited in development
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
@@ -48,7 +48,7 @@ app.use(limiter);
 app.use(express.static(publicDirPath));
 
 // Recursive function to load routes from nested folders
-export const routes: { path: string; method: string; time: number }[] = [];
+export const routes: {module: string; path: string; method: string; time: number }[] = [];
 
 const loadRoutes = (basePath: string, baseRoute: string) => {
   if (fs.existsSync(basePath)) {
@@ -83,6 +83,7 @@ const loadRoutes = (basePath: string, baseRoute: string) => {
                 );
                 methods.forEach((method) => {
                   routes.push({
+                  module: item.split('.')[0],
                     path: `${baseRoute}${layer.route.path}`,
                     method,
                     time: loadTime,
